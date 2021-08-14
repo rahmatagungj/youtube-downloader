824aa8f61f444f24bbf103b8a36e6575b59f9660
Rahmat Agung Julians <rahmatagungj@Gmail.com>
2021-08-14 16:33:35 +0700

    fixing for button and layouts

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -8,26 +8,42 @@ interface DetailsProps {
 
 const Details = ({ allData, setAllData }: DetailsProps) => {
   return (
-    <div>
-      <h1>{allData?.title}</h1>
-      <img src={allData?.thumbnails} alt={allData?.title} />
-      <p>{allData?.description}</p>
-      <a
-        href={allData?.audioUrl}
-        target="_blank"
-        rel="noopener noreferrer"
-        download
-      >
-        <button>Download Audio</button>
-      </a>
-      <a
-        href={allData?.videoUrl}
-        target="_blank"
-        rel="noopener noreferrer"
-        download
-      >
-        <button>Download Video</button>
-      </a>
+    <div className="p-5 md:p-0 flex justify-center">
+      <div className="bg-white shadow-xl p-3 rounded-lg w-full md:w-96">
+        <img
+          src={allData?.thumbnails}
+          alt={allData?.title}
+          className="rounded-lg"
+        />
+        <h1 className="font-bold text-lg text-gray-800">{allData?.title}</h1>
+        <p className="text-gray-800 text-md break-words ">
+          {allData?.description.split(" ").slice(0, 15).join(" ")}
+        </p>
+        <div className="flex justify-evenly items-center mt-2 flex-col md:flex-row">
+          <a
+            href={allData?.audioUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-full md:mr-2"
+            download
+          >
+            <button className="p-3 w-full flex-shrink font-semibold transition duration-500 ease-in-out transform bg-blue-500 mt-2 rounded-xl text-gray-50 hover:bg-blue-600">
+              Download Audio
+            </button>
+          </a>
+          <a
+            href={allData?.videoUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-full"
+            download
+          >
+            <button className="p-3 w-full font-semibold transition duration-500 ease-in-out transform bg-blue-500 mt-2 rounded-xl text-gray-50 hover:bg-blue-600">
+              Download Video
+            </button>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import UseDownload from "../hooks/useDownload";
+import MainLayout from "../layouts/MainLayout";
 import Details from "./Details";
 
 export interface IAllData {
@@ -21,7 +22,6 @@ const Home = () => {
     if (isError) setIsError(false);
     try {
       const { audio, video } = await UseDownload(url.current?.value.toString());
-      console.log(audio, video);
       setAllData({
         title: audio.result.title,
         description: audio.result.desc,
@@ -35,27 +35,66 @@ const Home = () => {
     setIsLoading(false);
   };
 
+  const RenderHome = () => {
+    return (
+      <React.Fragment>
+        <div className="container flex flex-col px-5 py-8 mx-auto lg:items-center">
+          <div className="flex flex-col w-full mb-8 text-left lg:text-center justify-center"></div>
+          <h1 className="mx-auto mb-6 text-2xl font-semibold leading-none tracking-tighter text-gray-100 lg:w-5/6 sm:text-6xl title-font">
+            Youtube Downloader
+          </h1>
+          <p className="mx-auto text-base font-medium leading-relaxed text-gray-200 lg:w-5/6 text-center">
+            Buat semuanya menjadi mudah dan cepat dalam mengunduh video dan
+            audio dari Youtube.
+          </p>
+          <div className="mt-10" />
+          {isError && (
+            <div className="p-2 justify-center flex flex-row">
+              <div className="inline-flex items-center leading-none text-white rounded-lg p-2 shadow text-teal text-sm">
+                <span className="inline-flex bg-red-600 text-white rounded-full h-6 px-3 justify-center items-center">
+                  Gagal
+                </span>
+                <span className="inline-flex px-2">Video tidak ditemukan</span>
+              </div>
+            </div>
+          )}
+          <div className="flex flex-col md:flex-row md:items-start w-full mx-auto justify-center lg:w-5/6">
+            <div className="relative md:mr-4 text-left lg:w-full xl:w-1/2 md:w-full">
+              <input
+                type="text"
+                id="urlsite"
+                placeholder="Masukan URL Video"
+                name="urlsite"
+                ref={url}
+                disabled={isLoading}
+                className="flex-grow w-full px-4 py-2 mb-4 md:mr-4 text-base text-black transition duration-650 ease-in-out transform rounded-lg bg-blueGray-200 focus:outline-none focus:border-purple-500 sm:mb-0 focus:bg-white focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
+              />
+            </div>
+            <button
+              onClick={handleDownload}
+              disabled={isLoading}
+              className={`${
+                isLoading && "cursor-not-allowed"
+              } mx-auto md:mx-0 w-32 text-center justify-center md:w-20 flex items-center px-6 py-2 mt-auto font-semibold text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-lg hover:bg-blue-700 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2`}
+            >
+              {isLoading ? "Memuat" : "Download"}
+            </button>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  };
   return (
-    <div>
-      {!allData && (
-        <>
-          <div className="home">home</div>
-          <input
-            type="text"
-            name="url"
-            id="url"
-            ref={url}
-            disabled={isLoading}
-          />
-          <button onClick={handleDownload} disabled={isLoading}>
-            {isLoading ? "Memuat" : "Download"}
-          </button>
-        </>
-      )}
-      {!isError && !isLoading && allData && (
-        <Details allData={allData} setAllData={setAllData} />
-      )}
-    </div>
+    <MainLayout>
+      <div className="flex flex-col h-screen">
+        <div className="m-auto">
+          {!allData && <RenderHome />}
+          {!isError && !isLoading && allData && (
+            <Details allData={allData} setAllData={setAllData} />
+          )}
+        </div>
+      </div>
+    </MainLayout>
   );
 };
 
